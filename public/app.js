f19fe9b78280ecf9847927b66a59c57cf5191ae1
rpenn <ryanpenn00@gmail.com>
2015-09-24 05:48:36 -0400

    posted answers using scope, per directions

---
+++ public/app.js
@@ -0,0 +1,15 @@
+var app = angular.module('FlashCards', []);
+
+app.controller('FlashCardController', function($scope) {
+	
+	$scope.flashCard = {
+    question: 'What is Angular?',
+    answers: [
+        { text: 'A front-end framework for great power!', correct: true },
+        { text: 'Something lame, who cares, whatever.', correct: false },
+        { text: 'Some kind of fish, right?', correct: false }
+    ]
+	};
+
+
+});
